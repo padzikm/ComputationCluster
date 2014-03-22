028cbbe29711b4f634050e31b9f2f52e10c61eee
Michal Padzik <padzikm@gmail.com>
2014-03-22 22:03:21 +0100

    Resolved confilcts

---
+++ ComputationalCluster/CommunicationServer/DVRPProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationServer
+{
+    [Synchronization]
+    static class DVRPProblem
+    {
+        public static List<int> Clients { get; set; } 
+        public static List<int> TaskSolvers { get; set; }
+        public static List<int> Nodes { get; set; } 
+        static DVRPProblem()
+        {
+            Clients = new List<int>();
+            TaskSolvers = new List<int>();
+            Nodes = new List<int>();
+        }
+    }
+}
